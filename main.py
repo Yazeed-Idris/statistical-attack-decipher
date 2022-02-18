abea194d9701ba8cb782feb532946097ae4b69aa
Yazeed Idris <s201730270@kfupm.edu.sa>
2022-02-18 21:40:09 +0300

    - Finished statistical attack

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def window(corpus, window_size):
 
 common_trigram = 'THE'
 common_trigram2 = 'AND'
-common_diagram = 'HE'
 
 common_letters = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'D', 'L', 'C', 'U', 'M', 'W', 'F', 'G', 'Y', 'P', 'B', 'V', 'K', 'J', 'X', 'Q', 'Z']
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -112,26 +111,8 @@ cipher_text = cipher_text.lower()
 MyList = cipher_text.split()
 my_dict = {i: MyList.count(i) for i in MyList}
 
-mixed_trigrams = window(cipher_text, 3)
+
 trigrams_list = {}
-for word in mixed_trigrams:
-    if len(word.replace(' ', '')) == 3:
-        trigrams_list[word] += 1
-
-
-print()
-
-end_with_ing = {}
-for word in my_dict:
-    if len(word) > 3:
-        end_with_ing[word[-3:]] = 0
-for word in my_dict:
-    if len(word) > 3:
-        end_with_ing[word[-3:]] += 1
-print(end_with_ing)
-
-
-
 for word, count in my_dict.items():
     if len(word) == 3:
         trigrams_list[word] = count
@@ -141,37 +122,57 @@ trigrams_list.pop(max_trigram1)
 max_trigram2 = max_in_dict(trigrams_list)
 trigrams_list.pop(max_trigram2)
 
-diagrams_list = {}
-
-for word, count in my_dict.items():
-    if len(word) == 2:
-        diagrams_list[word] = count
-# Finding the highest 2 frequencies in the trigrams and removing them from the list
-max_diagram1 = max_in_dict(diagrams_list)
-diagrams_list.pop(max_diagram1)
-# max_diagram2 = max_in_dict(diagrams_list)
-# diagrams_list.pop(max_diagram2)
-
-
+# Replacing trigram characters by the frequency
 for i in range(len(max_trigram1)):
     cipher_text = cipher_text.replace(max_trigram1[i], common_trigram[i])
     cipher_text = cipher_text.replace(max_trigram2[i], common_trigram2[i])
 
-# for i in range(len(max_diagram1)):
-#     cipher_text = cipher_text.replace(max_diagram1[i], common_diagram[i])
+bigrams_list = {}
+for word, count in my_dict.items():
+    if len(word) == 2:
+        bigrams_list[word] = count
+# Finding the highest 2 frequencies in the trigrams and removing them from the list
+max_bigram1 = max_in_dict(bigrams_list)
+bigrams_list.pop(max_bigram1)
+max_bigram2 = max_in_dict(bigrams_list)
+bigrams_list.pop(max_bigram2)
 
 # Calculate character frequencies
 charFrequincies = {}
 for i in range(len(alphabet)):
     charFrequincies[alphabet[i]] = cipher_text.count(alphabet[i])
-print(charFrequincies)
 
+# Replacing remaining characters by their frequencies
 for i in range(20):
     max_char = max_in_dict(charFrequincies)
     cipher_text.replace(max_char, common_letters[i])
     charFrequincies.pop(max_char)
 
+# Using trial and error to find the Diagram characters
+cipher_text = cipher_text.replace('x', 'O')
+cipher_text = cipher_text.replace('b', 'F')
 
+# Finding characters by filling missing characters from some words such as mNTENDED --> m is I
+cipher_text = cipher_text.replace('m', 'I')
+cipher_text = cipher_text.replace('q', 'S')
+cipher_text = cipher_text.replace('l', 'W')
+cipher_text = cipher_text.replace('i', 'L')
+cipher_text = cipher_text.replace('g', 'B')
+cipher_text = cipher_text.replace('h', 'R')
+cipher_text = cipher_text.replace('r', 'G')
+cipher_text = cipher_text.replace('a', 'C')
+cipher_text = cipher_text.replace('c', 'M')
+cipher_text = cipher_text.replace('z', 'U')
+cipher_text = cipher_text.replace('s', 'K')
+cipher_text = cipher_text.replace('e', 'P')
+cipher_text = cipher_text.replace('f', 'V')
+cipher_text = cipher_text.replace('j', 'Q')
+cipher_text = cipher_text.replace('k', 'X')
+cipher_text = cipher_text.replace('d', 'Y')
+cipher_text = cipher_text.replace('o', 'J')
+
+# by elimination w is differently Z
+cipher_text = cipher_text.replace('w', 'Z')
 
 print(cipher_text)
 
